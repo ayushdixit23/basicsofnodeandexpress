734bc48ebb90e5d5f8833ca18fbeb3078fd94f2a
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2023-09-18 23:25:41 +0530

    2nd commit

--- index.js
+++ index.js
@@ -9,82 +9,82 @@ const port = process.env.PORT || 3000;
 
 // creating a file
 
-// fs.writeFileSync("ayush.text", "Simple text file for testing")
-// fs.writeFileSync(myPath + "/fceghj.txt", "complex text file for testing")
-// for (let i = 0; i < 5; i++) {
-// 	fs.writeFileSync(myPath + "/myfile" + i + ".txt", "files created by loops this is my " + i + " file")
-// }
+fs.writeFileSync("ayush.text", "Simple text file for testing")
+fs.writeFileSync(myPath + "/fceghj.txt", "complex text file for testing")
+for (let i = 0; i < 5; i++) {
+	fs.writeFileSync(myPath + "/myfile" + i + ".txt", "files created by loops this is my " + i + " file")
+}
 
-// app.get('/', (req, res) => {
-// 	res.send("<h1>hello everyone</h1>")
-// })
+app.get('/', (req, res) => {
+	res.send("<h1>hello everyone</h1>")
+})
 
 // reading a file
 
-// fs.readFile(myPath + "/myfile.txt", "utf8", (err, item) => {
-// 	console.log(item)
-// })
+fs.readFile(myPath + "/myfile.txt", "utf8", (err, item) => {
+	console.log(item)
+})
 
 // renaming a file
 
-// fs.rename(myPath + "/apple.txt", myPath + "/file.txt", (err) => {
-// 	if (!err) {
-// 		console.log("files renamed")
-// 	}
-// })
+fs.rename(myPath + "/apple.txt", myPath + "/file.txt", (err) => {
+	if (!err) {
+		console.log("files renamed")
+	}
+})
 
 // updating a file
 
-// fs.appendFile(myPath + "/myfile.txt", " this are extended text.", (err) => {
-// 	if (!err) {
-// 		console.log("file updated")
-// 	}
-// })
+fs.appendFile(myPath + "/myfile.txt", " this are extended text.", (err) => {
+	if (!err) {
+		console.log("file updated")
+	}
+})
 
 // deleting a file
 
-// fs.unlinkSync(myPath + "/fceghj.txt")
+fs.unlinkSync(myPath + "/fceghj.txt")
 
 // basic routing
 
-// app.get('/about', (req, res) => {
-// 	res.send("hello about")
-// 	console.log(req.params())
-// })
+app.get('/about', (req, res) => {
+	res.send("hello about")
+	console.log(req.params())
+})
 
 // Sending a HTML file
 
-// app.get("/", (req, res) => {
-// 	res.sendFile(HTMLPath, (err) => {
-// 		if (!err) {
-// 			res.send("HTML file uploaded on web")
-// 		}
-// 	})
-// })
+app.get("/", (req, res) => {
+	res.sendFile(HTMLPath, (err) => {
+		if (!err) {
+			res.send("HTML file uploaded on web")
+		}
+	})
+})
 
-// app.get("/public", (req, res) => {
-// 	res.sendFile(HTMLPaths, (err) => {
-// 		if (!err) {
-// 			res.send("HTML file uploaded on web")
-// 		}
-// 	})
-// })
+app.get("/public", (req, res) => {
+	res.sendFile(HTMLPaths, (err) => {
+		if (!err) {
+			res.send("HTML file uploaded on web")
+		}
+	})
+})
 
 // Routing and Middleware
 
-// const middleWare = (req, res, next) => {
-// 	if (!req.query.age) {
-// 		res.send("Enter Your Age Params")
-// 	} else if (req.query.age <= 10) {
-// 		res.send("You need to be Above 10")
-// 	} else {
-// 		next()
-// 	}
-// }
+const middleWare = (req, res, next) => {
+	if (!req.query.age) {
+		res.send("Enter Your Age Params")
+	} else if (req.query.age <= 10) {
+		res.send("You need to be Above 10")
+	} else {
+		next()
+	}
+}
 
-// app.get('/ayush', middleWare, (req, res) => {
-// 	res.send("<h1>my name is ayush</h1>")
-// })
+app.get('/ayush', middleWare, (req, res) => {
+	res.send("<h1>my name is ayush</h1>")
+})
 
 app.listen(port);
 
